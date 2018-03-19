1ff3b1f190c8f282891ec8f90cbe7b3955f78fda
Bastian Zamorano <bizamorano@uc.cl>
2018-03-19 12:40:50 -0300

    init

--- db/schema.rb
+++ db/schema.rb
@@ -12,21 +12,17 @@
 
 ActiveRecord::Schema.define(version: 20180318202519) do
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
+    t.bigint "post_id"
+    t.text "user"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nombre"
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -36,7 +32,7 @@ ActiveRecord::Schema.define(version: 20180318202519) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -59,4 +55,6 @@ ActiveRecord::Schema.define(version: 20180318202519) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
 end
